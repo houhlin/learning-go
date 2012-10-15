b8c4003119646c9ed1a1363ffc3733da3b4f48ac
liangsdu <liangsdu@gmail.com>
2012-10-15 20:11:25 +0800

    Update 48.go

--- 48.go
+++ 48.go
@@ -2,18 +2,18 @@
 For cube roots, Newton's method amounts to repeatings:
 z = z - (z^3 - x) / (3*z^2)
 Find the cube roots of 2, just to make sure the algorithm works. There is a cmath.Pow function.
-*/
+update for go 1 */
 package main
 
 import (
 	"fmt"
-	"cmath"
+	"math/cmplx"
 )
 
 func Cbrt(x complex128) complex128 {
 	var z complex128 = 1.0
 	for i:=0; i<10; i++ {
-		z = z - (cmath.Pow(z,3)-x)/(3*cmath.Pow(z,2))
+		z = z - (cmplx.Pow(z,3)-x)/(3*cmplx.Pow(z,2))
 	}
 	return z
 }
